02c6a9a80e0685b0e1262031df6591651d2ceb28
Manh luong <manh.luong@amela.vn>
2022-02-22 23:25:59 +0700

    Refs create user

--- src/controllers/user_controller/user_controller.ts
+++ src/controllers/user_controller/user_controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import * as jwt from "jsonwebtoken";
 import config from "../../helpers/jwtConfig"
-import  { User }  from "../../models/user/user_model";
+import { User } from "../../models/user/user_model";
 import { errorHandler, StatusCodeException } from "../../middleware/error_middleware";
 import HttpException from "../../common/http-exception";
 import { Project } from "../../models/project/project_model";
@@ -28,34 +28,57 @@ class UserController {
       if (users.length === 0) {
         errorHandler(new HttpException(StatusCodeException.BAD_REQUEST, 'List user is empty'), req, res)
       } else {
-        res.status(StatusCodeException.SUCESS).send({status: StatusCodeException.SUCESS, users: users})
+        res.status(StatusCodeException.SUCESS).send({ status: StatusCodeException.SUCESS, users: users })
       }
     } catch (e) {
       errorHandler(new HttpException(StatusCodeException.BAD_REQUEST, e.message), req, res)
     }
   }
- //Get one User and Project of user
-    static getUser = async (req: Request, res: Response) => {
-        const id: number = parseInt(req.params.id);
-        try {
-          const user = await User.findAll({
-            attributes: [Colums.id, Colums.name, Colums.address, Colums.birthday, Colums.avatarUrl, Colums.role],
-            where: {
-              id: id
-            },
-            include: {
-              model: Project,
-              attributes: [Colums.id, Colums.name, Colums.createdAt, Colums.updatedAt, Colums.status]
-            }
-          });
-          if (user.length === 0 || !user) {
-            errorHandler(new HttpException(StatusCodeException.BAD_REQUEST, 'Cannot find user'), req, res)
-          } else {
-            res.status(StatusCodeException.SUCESS).send({status: StatusCodeException.SUCESS, user: user})
-          }
-        } catch (e) {
-           errorHandler(new HttpException(StatusCodeException.BAD_REQUEST, e.message), req, res)
+  //Get one User and Project of user
+  static getUser = async (req: Request, res: Response) => {
+    const id: number = parseInt(req.params.id);
+    try {
+      const user = await User.findAll({
+        attributes: [Colums.id, Colums.name, Colums.address, Colums.birthday, Colums.avatarUrl, Colums.role],
+        where: {
+          id: id
+        },
+        include: {
+          model: Project,
+          attributes: [Colums.id, Colums.name, Colums.createdAt, Colums.updatedAt, Colums.status]
         }
+      });
+      if (user.length === 0 || !user) {
+        errorHandler(new HttpException(StatusCodeException.BAD_REQUEST, 'Cannot find user'), req, res)
+      } else {
+        res.status(StatusCodeException.SUCESS).send({ status: StatusCodeException.SUCESS, user: user })
       }
+    } catch (e) {
+      errorHandler(new HttpException(StatusCodeException.BAD_REQUEST, e.message), req, res)
+    }
+  }
+
+  static createUser = async (req: Request, res: Response) => {
+    let hasPassword = `${md5(`"${req.body.password}"`)}`;
+    try {
+      const userExits = await User.findOne({ where: { username: req.body.username } })
+      if (!userExits) {
+        const user = await User.create(
+          {
+            username: req.body.username,
+            password: hasPassword,
+            name: req.body.name,
+            address: req.body.address,
+            company_id: req.body.company_id,
+            role: req.body.role
+          });
+        res.status(StatusCodeException.SUCESS).send({ status: StatusCodeException.SUCESS, user: user })
+      } else {
+        errorHandler(new HttpException(StatusCodeException.BAD_REQUEST, 'User exits'), req, res)
+      }
+    } catch (e) {
+      errorHandler(new HttpException(StatusCodeException.BAD_REQUEST, e.message), req, res)
+    }
+  }
 }
 export default UserController;
