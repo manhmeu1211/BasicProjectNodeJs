6353ce7a2762b903549da09ca70a5b5183cb705d
Manh luong <manh.luong@amela.vn>
2022-02-22 22:17:23 +0700

    Refs create entity

---
+++ src/models/project/project_model.ts
@@ -0,0 +1,20 @@
+import { dbConfig } from "../../helpers/dbconfig";
+import { Sequelize, Model, DataTypes } from 'sequelize'
+import { User } from "../user/user_model";
+import { Company } from "../company/company_model";
+
+export const Project = dbConfig.define("projects", {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.TEXT,
+  },
+  company_id: {
+    allowNull: false,
+    type: DataTypes.INTEGER
+  }
+});
